8632bf5b9edd03b596cf37ab61dd1f9d2e035cd4
Jonathan Haber <jhaber@hubspot.com>
2018-02-15 10:23:56 -0500

    Test tweaks

--- src/test/java/com/hubspot/jackson/datatype/protobuf/builtin/RepeatedListValueTest.java
+++ src/test/java/com/hubspot/jackson/datatype/protobuf/builtin/RepeatedListValueTest.java
@@ -1,24 +1,28 @@
 package com.hubspot.jackson.datatype.protobuf.builtin;
 
 import static com.hubspot.jackson.datatype.protobuf.util.ObjectMapperHelper.camelCase;
+import static org.assertj.core.api.Assertions.assertThat;
 
 import java.io.IOException;
 
 import org.junit.Test;
 
+import com.google.protobuf.ListValue;
+import com.google.protobuf.Value;
 import com.hubspot.jackson.datatype.protobuf.util.BuiltInProtobufs.RepeatedListValue;
 
 public class RepeatedListValueTest {
+  private static final Value NESTED = Value.newBuilder().setStringValue("nested").build();
+  private static final Value LIST = Value
+          .newBuilder()
+          .setListValue(ListValue.newBuilder().addValues(NESTED).build())
+          .build();
 
   @Test
   public void itReadsNestedListValues() throws IOException {
     String json = "{\"listValues\":[[\"nested\"]]}";
     RepeatedListValue message = camelCase().readValue(json, RepeatedListValue.class);
-  }
-
-  @Test
-  public void itReadsMixedTypeValues() throws IOException {
-    String json = "{\"listValues\":[null,1.5,\"test\",true,{\"key\":\"value\"},[\"nested\"]]}";
-    RepeatedListValue message = camelCase().readValue(json, RepeatedListValue.class);
+    assertThat(message.getListValuesCount()).isEqualTo(1);
+    assertThat(message.getListValues(0)).isEqualTo(LIST);
   }
 }
