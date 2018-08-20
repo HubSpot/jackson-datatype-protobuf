2931055d1963e8b699ad4eeebe537d8b50a87e23
Jonathan Haber <jhaber@hubspot.com>
2018-08-20 13:52:32 -0400

    Special-case PropertyNamingStrategy.LOWER_CAMEL_CASE

--- src/main/java/com/hubspot/jackson/datatype/protobuf/PropertyNamingStrategyWrapper.java
+++ src/main/java/com/hubspot/jackson/datatype/protobuf/PropertyNamingStrategyWrapper.java
@@ -7,12 +7,15 @@ import com.google.common.base.CaseFormat;
 @SuppressWarnings("serial")
 public class PropertyNamingStrategyWrapper extends PropertyNamingStrategyBase {
   private static final PropertyNamingStrategyBase SNAKE_TO_CAMEL = new SnakeToCamelNamingStrategy();
+  private static final PropertyNamingStrategyBase NO_OP = new NoOpNamingStrategy();
 
   private final PropertyNamingStrategyBase delegate;
 
   public PropertyNamingStrategyWrapper(PropertyNamingStrategy delegate) {
     if (delegate instanceof PropertyNamingStrategyBase) {
       this.delegate = (PropertyNamingStrategyBase) delegate;
+    } else if (delegate == PropertyNamingStrategy.LOWER_CAMEL_CASE) {
+      this.delegate = NO_OP;
     } else {
       this.delegate = SNAKE_TO_CAMEL;
     }
@@ -30,5 +33,13 @@ public class PropertyNamingStrategyWrapper extends PropertyNamingStrategyBase {
       return CaseFormat.LOWER_UNDERSCORE.to(CaseFormat.LOWER_CAMEL, fieldName);
     }
 
+  }
+
+  private static class NoOpNamingStrategy extends PropertyNamingStrategyBase {
+
+    @Override
+    public String translate(String fieldName) {
+      return fieldName;
+    }
   }
 }
